133f3761411fd3f73effadc56808c1a0112721a5
garv Virginkar <100025840@mvla.net>
2022-01-25 11:15:46 -0800

    can now navigate to the Open parli results page

--- src/scraper.py
+++ src/scraper.py
@@ -3,13 +3,21 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_experimental_option(
+    "prefs", {'profile.managed_default_content_settings.javascript': 2})
+
 
 PATH = "/home/garv/chromedriver_linux64/chromedriver"
-driver = webdriver.Chrome(PATH)
+driver = webdriver.Chrome(PATH, chrome_options=chrome_options)
 
 driver.get("https://www.tabroom.com/index/index.mhtml")
 searchQuery = driver.find_element_by_name("search")
-searchQuery.send_keys("National Parliamentary")
+tournamentName = "James Logan"
+searchQuery.send_keys(tournamentName)
 searchQuery.send_keys(Keys.RETURN)
 
 main = WebDriverWait(driver, 10).until(
@@ -25,10 +33,20 @@ try:
     try:
         tourneyResults = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located(
-                (By.XPATH, "/html/body/div[1]/div[2]/div/div[2]/ul[1]/li[6]/a"))
+                (By.XPATH, "/html/body/div/div[2]/div/div[1]/ul/li[6]/a"))
             )
         tourneyResults.click()
+        try:
+            select = Select(
+                driver.find_element_by_xpath("/html/body/div/div[2]/div/div[1]/div[1]/form/div/span[1]/select"))
+            select.select_by_visible_text("Parliamentary Debate")
+            button = driver.find_element_by_xpath(
+                "/html/body/div/div[2]/div/div[1]/div[1]/form/div/span[2]/input")
+            button.click()
+            #selector.click()
+        except:
+            driver.quit()
     except:
         driver.quit()
 except:
-    print()
+    driver.quit()
