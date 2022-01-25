3285e6a7c293564777700bdbbf8424fc7d321f2a
garv Virginkar <100025840@mvla.net>
2022-01-25 13:08:33 -0800

    navigates to the prelim seed page for a tournament given the tournament name

--- src/scraper.py
+++ src/scraper.py
@@ -6,25 +6,26 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.options import Options
 
+# driver setup
 chrome_options = Options()
 chrome_options.add_experimental_option(
     "prefs", {'profile.managed_default_content_settings.javascript': 2})
-
-
 PATH = "/home/garv/chromedriver_linux64/chromedriver"
 driver = webdriver.Chrome(PATH, chrome_options=chrome_options)
 
+# search for tournament
 driver.get("https://www.tabroom.com/index/index.mhtml")
 searchQuery = driver.find_element_by_name("search")
-tournamentName = "James Logan"
+tournamentName = "National Parliamentary Debate Invitational"
 searchQuery.send_keys(tournamentName)
 searchQuery.send_keys(Keys.RETURN)
 
+# find tournament
 main = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.ID, "search_results")))
 divTags = main.find_elements_by_tag_name("tr")
 
-divTag = divTags[1]
+divTag = divTags[1]  # index 1 not 0. 0 is the titlebar
 try:
     tag = divTag.find_element_by_tag_name("td")
     tourneyName = tag.find_element_by_tag_name(
@@ -39,11 +40,16 @@ try:
         try:
             select = Select(
                 driver.find_element_by_xpath("/html/body/div/div[2]/div/div[1]/div[1]/form/div/span[1]/select"))
-            select.select_by_visible_text("Parliamentary Debate")
+            select.select_by_visible_text("Open Parli")
             button = driver.find_element_by_xpath(
                 "/html/body/div/div[2]/div/div[1]/div[1]/form/div/span[2]/input")
             button.click()
-            #selector.click()
+            try:
+                preSeeds = driver.find_element_by_xpath(
+                    "/html/body/div/div[2]/div/div[1]/div[2]/a[1]")
+                preSeeds.click()
+            except:
+                driver.quit()
         except:
             driver.quit()
     except:
